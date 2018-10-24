dd2a3db2f59ee6be0e74c8abc8fcf535f4dcd9ea
manu88 <manuel.deneu@gmail.com>
2018-10-24 16:58:08 +0200

    Added open/close/read/write sys calls

---
+++ projects/init/src/SysCalls/OpenClose.c
@@ -0,0 +1,27 @@
+#include "../SysCalls.h"
+
+
+
+int handle_read(InitContext* context, Process *senderProcess, seL4_MessageInfo_t message)
+{
+	return 0;
+}
+
+int handle_write(InitContext* context, Process *senderProcess, seL4_MessageInfo_t message)
+{ 
+        return 0;
+}
+
+
+int handle_open(InitContext* context, Process *senderProcess, seL4_MessageInfo_t message)
+{ 
+        return 0;
+}
+
+
+int handle_close(InitContext* context, Process *senderProcess, seL4_MessageInfo_t message)
+{ 
+        return 0;
+}
+
+
