97eef60489514f706b2939799749ebe52ccde376
Manuel Deneu <manueldeneu@gmail.com>
2018-10-26 10:58:03 +0200

    Added SysCall app unit tests

---
+++ projects/apps/TestSysCalls/src/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TestSysCalls
+//
+//  Created by Manuel Deneu on 26/10/2018.
+//  Copyright © 2018 Manuel Deneu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
