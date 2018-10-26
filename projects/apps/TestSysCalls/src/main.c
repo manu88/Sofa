6068f3fa30732f8145d376cb3f30473b2ec1e1a5
Manuel Deneu <manueldeneu@gmail.com>
2018-10-26 11:13:04 +0200

    Test syscalls update

--- projects/apps/TestSysCalls/src/main.c
+++ projects/apps/TestSysCalls/src/main.c
@@ -7,9 +7,42 @@
 //
 
 #include <stdio.h>
+#include <assert.h>
+#include <sys/fcntl.h>
+#include <errno.h>
+#include <unistd.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+static int doOpenTests(void);
+static int doGetPidTests(void);
+
+static int doGetPidTests()
+{
+    errno = 0;
+    assert(getpid() > 0);
+    assert(errno == 0);
+    
+    errno = 0;
+    assert(getppid() > 0);
+    assert(errno == 0);
+    return 1;
+}
+static int doOpenTests()
+{
+    errno = 0;
+    assert(open(NULL, 0) == -1);
+    assert(errno == EFAULT);
+    
+    errno = 0;
+    assert(open("", 0) == -1);
+    assert(errno == ENOENT);
+    
+    
+    return 1;
+}
+int main(int argc, const char * argv[])
+{
+    assert(doGetPidTests());
+    assert(doOpenTests());
+    
     return 0;
 }
