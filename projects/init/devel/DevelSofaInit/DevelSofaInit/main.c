76eec60ce7d41701aa0bc8539962967d87c6b690
Manuel Deneu <manueldeneu@gmail.com>
2018-10-29 10:02:12 +0100

    Added Devel project for init

---
+++ projects/init/devel/DevelSofaInit/DevelSofaInit/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DevelSofaInit
+//
+//  Created by Manuel Deneu on 29/10/2018.
+//  Copyright © 2018 Manuel Deneu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
