1a263536c6ea1dc6d6a2e3d5b4a56a18ffa9098e
Manuel Deneu <manueldeneu@gmail.com>
2018-11-09 17:37:49 +0100

    FileServer/inode update part I

---
+++ projects/init/devel/UnitTests/DevServer_UnitTests.c
@@ -0,0 +1,9 @@
+//
+//  DevServer_UnitTests.c
+//  DevelSofaInit
+//
+//  Created by Manuel Deneu on 09/11/2018.
+//  Copyright © 2018 Manuel Deneu. All rights reserved.
+//
+
+#include "DevServer_UnitTests.h"
---
+++ projects/init/devel/UnitTests/DevServer_UnitTests.h
@@ -0,0 +1,14 @@
+//
+//  DevServer_UnitTests.h
+//  DevelSofaInit
+//
+//  Created by Manuel Deneu on 09/11/2018.
+//  Copyright © 2018 Manuel Deneu. All rights reserved.
+//
+
+#ifndef DevServer_UnitTests_h
+#define DevServer_UnitTests_h
+
+#include <stdio.h>
+
+#endif /* DevServer_UnitTests_h */
