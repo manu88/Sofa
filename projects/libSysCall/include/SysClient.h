82d66b5a26dd4abb4e5b4a2aac5f95cef44af4a0
Manuel Deneu <manueldeneu@gmail.com>
2018-11-13 13:07:14 +0100

    Added RPC methods

--- projects/libSysCall/include/SysClient.h
+++ projects/libSysCall/include/SysClient.h
@@ -1,3 +1,14 @@
 #pragma once
+#include <sel4/types.h>
+
 
 int SysClientInit(int argc , char* argv[] );
+
+
+
+// IPC Part
+
+int IPCMessageInit(void);
+
+void IPCPushWord(seL4_Word word);
+void IPCPushString(const char* str);
