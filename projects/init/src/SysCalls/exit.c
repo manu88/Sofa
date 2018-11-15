12f774190510d50d394891d706722bfd3dbe952d
manu88 <manuel.deneu@gmail.com>
2018-11-15 16:48:35 +0100

    Syscall file names update

---
+++ projects/init/src/SysCalls/exit.c
@@ -0,0 +1,41 @@
+/*
+ * This file is part of the Sofa project
+ * Copyright (c) 2018 Manuel Deneu.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../SysCalls.h"
+#include <SysCallNum.h>
+#include <assert.h>
+#include "../FileServer.h"
+#include <fcntl.h>
+#include "../ProcessTable.h"
+
+int handle_exit(InitContext* context, Process *senderProcess, seL4_MessageInfo_t message)
+{
+    int exitStatus = seL4_GetMR(1);
+    printf("Got exit from %i status %i\n", senderProcess->_pid , exitStatus);
+
+    ProcessSignalStop( senderProcess);
+    ProcessDoCleanup( senderProcess);
+    if(!ProcessTableRemove( senderProcess))
+    {
+        printf("Unable to remove process!\n");
+    }
+    ProcessStop(context , senderProcess);
+    ProcessRelease(senderProcess);
+
+    printf("Init : Got %i processes \n" , ProcessTableGetCount() );
+    return 0;
+}
