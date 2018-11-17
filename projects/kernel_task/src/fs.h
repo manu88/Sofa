b5cc750e5bb64107c744ea75f21417c103b3d817
Manuel Deneu <manueldeneu@gmail.com>
2018-11-17 12:56:02 +0100

    INodeType now follows DT_* type

--- projects/kernel_task/src/fs.h
+++ projects/kernel_task/src/fs.h
@@ -33,16 +33,31 @@
 #include "TimerWheel/queue.h"
 #include <data_struct/chash.h>
 #include "uthash.h"
-
+#include <sys/dirent.h>
+/*
+ * INodeType should follow
+ */
+/*
+#define    DT_UNKNOWN     0
+#define    DT_FIFO         1
+#define    DT_CHR         2
+#define    DT_DIR         4
+#define    DT_BLK         6
+#define    DT_REG         8
+#define    DT_LNK        10
+#define    DT_SOCK        12
+#define    DT_WHT        14
+*/
 typedef enum
 {
-    InodeType_Invalid = 0,
-    INodeType_File    = 1,
-    INodeType_Folder  = 2,
+    InodeType_Invalid = DT_UNKNOWN,
+    INodeType_File    = DT_REG,
+    INodeType_Folder  = DT_DIR,
     
 } INodeType;
-struct _inode;
 
+
+struct _inode;
 
 struct _fileOperations
 {
