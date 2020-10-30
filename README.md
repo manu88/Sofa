603ffcbf6f15c198333c7754f475ab1eee043147
Manuel deneu <manuel.deneu@nokia-bell-labs.com>
2020-10-30 18:28:30 +0100

    readme + update.sh update

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 Sofa is build atop SeL4 microkernel and provides a set of userland servers and an API to manipulate them.
 
+## Build
+You should have all the tools and external deps required to build seL4 (some info [here](https://docs.sel4.systems/projects/buildsystem/host-dependencies.html)).
+
+Then call the update.sh script to fetch all the required components (seL4 kernel, libs, etc.)
+
+```
+sh update.sh
+mkdir build
+cd build
+../init-build.sh  -DPLATFORM=x86_64 -DSIMULATION=TRUE -DRELEASE=FALSE
+ninja
+./simulate
+```
+
 ## Status
 for now I'm trying to understand more the [sel4test](https://github.com/seL4/sel4test) repo and the [libsel4osapi](https://github.com/rticommunity/libsel4osapi) repo in order to create a process model with memory management.
 
---
+++ update.sh
@@ -0,0 +1,57 @@
+echo "==> Getting seL4 kernel"
+# SeL4 kernel repo
+if [ -d "kernel" ]; then
+	git -C kernel/ pull
+else 
+	git clone https://github.com/seL4/seL4.git kernel
+fi
+
+
+echo "==> Getting seL4 build tools"
+# SeL4 build tools
+mkdir -p tools
+
+if [ -d "tools/seL4" ]; then
+        git -C tools/seL4 pull
+else 
+	git clone https://github.com/seL4/seL4_tools.git tools/seL4
+fi
+
+echo "==> Getting seL4 util_libs"
+if [ -d "projects/util_libs/" ]; then
+	git -C projects/util_libs/ pull
+else
+	git clone https://github.com/seL4/util_libs.git projects/util_libs
+fi
+
+echo "==> Getting seL4 musllibc"
+if [ -d "projects/musllibc/" ]; then
+        git -C projects/musllibc/ pull
+else 
+	git clone https://github.com/seL4/musllibc.git projects/musllibc
+fi
+
+
+echo "==> Getting seL4 seL4_libs"
+if [ -d "projects/seL4_libs/" ]; then
+        git -C projects/seL4_libs/ pull
+else 
+	git clone https://github.com/seL4/seL4_libs.git projects/seL4_libs
+fi
+
+echo "==> Getting seL4 sel4runtime"
+if [ -d "projects/sel4runtime/" ]; then
+        git -C projects/sel4runtime/ pull
+else
+        git clone https://github.com/SEL4PROJ/sel4runtime.git projects/sel4runtime
+fi
+
+# create some links
+
+if [ ! -f "init-build.sh" ]; then
+	ln -s tools/seL4/cmake-tool/init-build.sh init-build.sh
+fil
+
+if [ ! -f "easy-settings.cmake" ]; then
+	ln -s projects/Sofa/easy-settings.cmake easy-settings.cmake
+fi
