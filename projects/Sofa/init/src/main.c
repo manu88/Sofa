bfffc9e3c5d4a2a461aeb9db82bd63ad5c72d6e2
manu88 <>
2020-11-15 13:51:18 +0100

    re-add init process

---
+++ projects/Sofa/init/src/main.c
@@ -0,0 +1,17 @@
+#include <allocman/vka.h>
+#include <allocman/bootstrap.h>
+
+
+#include <Sofa.h>
+#include <Thread.h>
+#include <runtime.h>
+
+int main(int argc, char *argv[])
+{
+    RuntimeInit(argc, argv);
+    
+    
+
+    return 1;
+}
+
