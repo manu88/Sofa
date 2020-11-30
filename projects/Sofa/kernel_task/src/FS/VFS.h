2ee2eb9bf09fe8ea19c9bc36e4e805ea17147464
manu88 <manuel.deneu@gmail.com>
2020-11-30 11:11:50 +0100

    VFS update

--- projects/Sofa/kernel_task/src/FS/VFS.h
+++ projects/Sofa/kernel_task/src/FS/VFS.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "IODevice.h"
 
+
+typedef enum
+{
+    VFSSupported_Unknown = 0,
+    VFSSupported_EXT2,
+}VFSSupported;
+
 int VFSInit(void);
 
 int VFSAddDEvice(IODevice *dev);
