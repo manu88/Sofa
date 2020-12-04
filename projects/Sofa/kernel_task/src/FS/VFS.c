0bbcc7adeb5325b3ff7419c30faf7871a4382f14
manu88 <manuel.deneu@gmail.com>
2020-12-04 09:27:26 +0100

    sync before rewrite virtio-blk

--- projects/Sofa/kernel_task/src/FS/VFS.c
+++ projects/Sofa/kernel_task/src/FS/VFS.c
@@ -1,42 +1,77 @@
 #include <stdio.h>
+#include <string.h>
 #include "VFS.h"
 #include "ext2.h"
+
 
 int VFSInit()
 {
     return 0;
 }
 
+
+static void testBlk(IODevice* dev)
+{
+
+    uint8_t str[512];// = "Hello this is some data";
+    for (int i=0;i<512;i++)
+    {
+        str[i] = i;
+    }
+    printf("--- WRITE ---\n");
+    IODeviceWrite(dev, 88, str, 512);
+    printf("DID Write\n");
+
+
+    uint8_t b[512];
+    memset(b, 0, 512);
+    printf("--- READ ---\n");
+    ssize_t ret = IODeviceRead(dev, 88, b, 512);
+    printf("READ return %zi\n",ret);
+    for(int i=0;i<100;i++)
+    {
+        printf(" %i ", b[i]);
+    }
+    printf("\n");
+}
+
 static int _VFSCheckSuperBlock(IODevice* dev, VFSSupported* fsType)
 {
     assert(fsType);
-    struct ext2_superblock sb;
-    printf("Read super block size is %zi\n", sizeof(struct ext2_superblock));
+    ext2_superblock_t sb;
 
-    ssize_t ret = IODeviceRead(dev, 2, &sb, sizeof(struct ext2_superblock));
-    printf("Superblock read %zi\n",ret);
+    ssize_t ret = IODeviceRead(dev, 2, &sb, sizeof(ext2_superblock_t));
+    printf("Superblock size %zi\n" ,sizeof(ext2_superblock_t));
     if(ret <=0)
     {
         printf("UNable to read super block on device '%s' error = %i\n", dev->name, ret);
         return -1;
     }
 
+    printf("Ext2 version %i.%i\n", sb.major_version, sb.minor_version);
 
-    printf("'%s'\n", sb.path_last_mounted_to); 
+    uint32_t blocksize = 1024 << sb.blocksize_hint;
+    printf("Block size is %u\n",  blocksize);
+    printf("Super block is at %u\n", sb.superblock_id);
 
-    for (int j=0;j<16;j++)
+
+    if(sb.ext2_sig != EXT2_SIGNATURE)
     {
-        printf("%X ", sb.fsid[j]);
-    }
-    printf("\n"); 
-
-
-    if(sb.ext2_signature != 0xef53)
-    {
-        printf("Ext2 signature does'nt match 0XEF53. got %X\n", sb.ext2_signature);
+        printf("Ext2 signature does'nt match 0XEF53. got %X\n", sb.ext2_sig);
         return -2;
     }
     *fsType = VFSSupported_EXT2;
+
+    uint32_t block_bgdt = sb.superblock_id + (sizeof(ext2_superblock_t) / blocksize);
+    printf("block_bgdt %u\n", block_bgdt);
+
+    uint8_t buff[512];
+    ret = IODeviceRead(dev, 4, &buff, 512);
+
+    printf("ret for bgdt %zi\n", ret);
+    ext2_block_group_desc_t *bgdt = buff;
+    printf("Starting block addr %zi \n", bgdt->block_of_inode_table);
+
     return 0;
 }
 
@@ -44,6 +79,10 @@ static int _VFSCheckSuperBlock(IODevice* dev, VFSSupported* fsType)
 int VFSAddDEvice(IODevice *dev)
 {
     printf("[VFS] add device '%s'\n", dev->name);
+
+    printf("TEST 1\n");
+    testBlk(dev);
+    return -1;
 
     VFSSupported type = VFSSupported_Unknown;
     int test = _VFSCheckSuperBlock(dev, &type);
--- projects/Sofa/kernel_task/src/FS/ext2.h
+++ projects/Sofa/kernel_task/src/FS/ext2.h
@@ -1,6 +1,50 @@
 #pragma once
 #include <sys/types.h>
 
+
+#define EXT2_SIGNATURE 0xEF53
+
+typedef struct {
+	uint32_t inodes;
+	uint32_t blocks;
+	uint32_t reserved_for_root;
+	uint32_t unallocatedblocks;
+	uint32_t unallocatedinodes;
+	uint32_t superblock_id;
+	uint32_t blocksize_hint; // shift by 1024 to the left
+	uint32_t fragmentsize_hint; // shift by 1024 to left
+	uint32_t blocks_in_blockgroup;
+	uint32_t frags_in_blockgroup;
+	uint32_t inodes_in_blockgroup;
+	uint32_t last_mount;
+	uint32_t last_write;
+	uint16_t mounts_since_last_check;
+	uint16_t max_mounts_since_last_check;
+	uint16_t ext2_sig; // 0xEF53
+	uint16_t state;
+	uint16_t op_on_err;
+	uint16_t minor_version;
+	uint32_t last_check;
+	uint32_t max_time_in_checks;
+	uint32_t os_id;
+	uint32_t major_version;
+	uint16_t uuid;
+	uint16_t gid;
+	uint8_t unused[940];
+} __attribute__((packed)) ext2_superblock_t;
+
+
+typedef struct {
+	uint32_t block_of_block_usage_bitmap;
+	uint32_t block_of_inode_usage_bitmap;
+	uint32_t block_of_inode_table;
+	uint16_t num_of_unalloc_block;
+	uint16_t num_of_unalloc_inode;
+	uint16_t num_of_dirs;
+	uint8_t unused[14];
+} __attribute__((packed)) ext2_block_group_desc_t;
+
+#if 0
 // comments from https://wiki.osdev.org/Ext2#Superblock
 struct ext2_superblock
 {
@@ -62,3 +106,6 @@ struct ext2_superblock
     uint32_t    head_of_orphan_inode_list;  // Head of orphan inode list
 } __packed;
 
+
+
+#endif
