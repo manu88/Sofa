eca004448217e14af6f93c6c465b8008e35aced9
manu88 <manuel.deneu@gmail.com>
2020-12-16 21:29:41 +0100

    add readdir in fakefs

--- projects/Sofa/kernel_task/src/FS/fakefs.c
+++ projects/Sofa/kernel_task/src/FS/fakefs.c
@@ -1,7 +1,10 @@
 #include "fakefs.h"
+#include "Log.h"
 #include <stdio.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <dirent.h>
+#include <assert.h>
 
 
 static int fakeFSStat(VFSFileSystem *fs, const char **path, int numPathSegments, VFS_File_Stat *stat);
@@ -48,12 +51,41 @@ static const FakeFile const files[] =
         .mode = O_RDONLY
     },
     {
+        .name = "file3",
+        .content = "Hello this is the content of file1",
+        .mode = O_RDONLY
+    },
+    {
+        .name = "file4",
+        .content = "Hello this is the content of file2, wich is a little bit longuer in order to test the buffers.\nPlease Note That this sentence should begin at a new line.\n\tItem1\n\tItem2",
+        .mode = O_RDONLY
+    },
+    {
+        .name = "file5",
+        .content = "Hello this is the content of file1",
+        .mode = O_RDONLY
+    },
+    {
+        .name = "file6",
+        .content = "Hello this is the content of file2, wich is a little bit longuer in order to test the buffers.\nPlease Note That this sentence should begin at a new line.\n\tItem1\n\tItem2",
+        .mode = O_RDONLY
+    },
+    {
+        .name = "file7",
+        .content = "Hello this is the content of file1",
+        .mode = O_RDONLY
+    },
+    {
+        .name = "file8",
+        .content = "Hello this is the content of file2, wich is a little bit longuer in order to test the buffers.\nPlease Note That this sentence should begin at a new line.\n\tItem1\n\tItem2",
+        .mode = O_RDONLY
+    },
+    {
         .name = "cons",
-//        .content = "Hello this is the content of file2, wich is a little bit longuer in order to test the buffers.\nPlease Note That this sentence should begin at a new line.\n\tItem1\n\tItem2",
         .mode = O_WRONLY
     },
 };
-#define NumFiles 3
+#define NumFiles 9
 
 VFSFileSystem* getFakeFS()
 {
@@ -79,8 +111,49 @@ static int fakeFSStat(VFSFileSystem *fs, const char **path, int numPathSegments,
     return ENOENT;
 }
 
+static int _ReadDir(File *file, void *buf, size_t numBytes)
+{
+    size_t remainFilesToList = file->size - file->readPos;
+    size_t numDirentPerBuff = numBytes / sizeof(struct dirent);
+    size_t numOfDirents = numDirentPerBuff > remainFilesToList? remainFilesToList:numDirentPerBuff;
+   
+    struct dirent *dirp = buf;
+
+    size_t nextOff = 0;
+    size_t acc = 0;
+
+    for(int i=0;i<numOfDirents;i++)
+    {
+        struct dirent *d = dirp + i;
+        size_t fPos = i + file->readPos;
+        assert(fPos <NumFiles);
+        snprintf(d->d_name, 256, "%s", files[fPos].name);
+        acc += sizeof(struct dirent);
+        d->d_off = acc;
+        d->d_type = DT_DIR;
+        d->d_reclen = sizeof(struct dirent);
+
+        if(i >= numOfDirents)
+        {
+            break;
+        }
+
+    }
+    file->readPos += numOfDirents;
+    return acc;
+}
+
+static FileOps _rootFOP = {.Read = _ReadDir};
+
 static int fakeFSOpen(VFSFileSystem *fs, const char *path, int mode, File *file)
 {
+    if(strcmp(path, "/") == 0)
+    {
+
+            file->ops = &_rootFOP;
+        file->size = NumFiles;
+        return 0;
+    }
     const char* p = path+1;
 
     for(int i=0;i<NumFiles;i++)
