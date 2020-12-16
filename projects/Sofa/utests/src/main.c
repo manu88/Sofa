905b0452f873664c492899c3f0360b0975e7ce09
manu88 <manuel.deneu@gmail.com>
2020-12-16 15:36:50 +0100

    up

--- projects/Sofa/utests/src/main.c
+++ projects/Sofa/utests/src/main.c
@@ -5,18 +5,26 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/mman.h>
 
-
-
-int main(int argc, char *argv[])
+static void testMmap()
 {
-    
-    RuntimeInit2(argc, argv);
-    VFSClientInit();
+    char* ptr = mmap(NULL, 4096, 0, 0,-1, 0);
+    assert(ptr);
+    ptr[0] = 1;
+    ptr[4095] = 2;
+    int ret = munmap(ptr, 4096);
+    assert(ret == 0);
+}
 
-    SFPrintf("Unit tests started\n");
-    assert(0);
-
+static void testMalloc(void)
+{
+    void *ptr = malloc(512);
+    assert(ptr);
+    free(ptr);
+}
+static void testRead(void)
+{
     int h = open("/fake/file1", O_RDONLY);
     assert(h != -1);
 
@@ -24,18 +32,21 @@ int main(int argc, char *argv[])
     ssize_t ret = read(h, b, 10);
     while (ret != EOF)
     {
-        SFPrintf("read returned %zi '%s'\n", ret, b);
-
         ret = read(h, b, 10);
         if(ret > 0)
             b[ret] = 0;
     }
     close(h);
-    
-    
+}
 
-    //void *t = dlopen("Some file", RTLD_NOW);
+int main(int argc, char *argv[])
+{
+    
+    RuntimeInit2(argc, argv);
+    VFSClientInit();
 
+    testRead();
+    testMmap();
     
     return 0;
 }
